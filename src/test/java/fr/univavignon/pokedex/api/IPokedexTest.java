7194414e28ad91f62e817ce3e8d125d275a75ab3
Romain Rigaud <romain.rigaud@alumni.univ-avignon.fr>
2024-02-06 18:14:06 +0100

    Ajout Tests

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -0,0 +1,18 @@
+package fr.univavignon.pokedex.api;
+
+import junit.framework.TestCase;
+
+public class IPokedexTest extends TestCase {
+
+    public void testSize() {
+    }
+
+    public void testAddPokemon() {
+    }
+
+    public void testGetPokemon() {
+    }
+
+    public void testGetPokemons() {
+    }
+}
